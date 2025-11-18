3df1a585d71837c6ba0ab94c3d1629baa84ac9c5
Paras Limbu <namshang715@gmail.com>
2025-11-18 14:09:02 +0545

    next change
    ....

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/my-app'>
       <App />
     </BrowserRouter>
   </StrictMode>,
